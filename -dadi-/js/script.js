4abf376bea6ba91851d7d8d051ba119068403ac5
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-23 17:46:34 +0100

    add button and h1

--- -dadi-/js/script.js
+++ -dadi-/js/script.js
@@ -4,8 +4,19 @@
 2. Confrontare risultati.
 */
 
+// Generatore numeri
 let utente = Math.ceil(Math.random() * 6);
 let pc = Math.ceil(Math.random() * 6);
-
 console.log('utente', utente, 'pc', pc);
+
+// Bottone
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+
+  let message = `Dado Utente = ${utente} <br>
+  Dado Pc = ${pc}`;
+
+  const dadi = document.getElementById('dadi').innerHTML = message;
+  
+});
 
